536c124c2d1899c6a1df227c36054feac0147b24
Nitin-Singh18 <i.nitinsingh18@gmail.com>
2023-12-27 13:04:40 +0530

    fea - dynamic app icon

--- app/src/main/java/com/example/dynamicappicon/MainActivity.kt
+++ app/src/main/java/com/example/dynamicappicon/MainActivity.kt
@@ -1,43 +1,73 @@
 package com.example.dynamicappicon
 
+import android.app.Activity
+import android.content.ComponentName
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.dynamicappicon.ui.theme.DynamicAppIconTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val mainActivityAlias = "${applicationContext.packageName}.MainActivityAlias"
+            val mainActivity = "${applicationContext.packageName}.MainActivity"
             DynamicAppIconTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
+                Column(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Greeting("Android")
+                    Button(onClick = {
+                        changeEnabledComponent(
+                            mainActivity,
+                            mainActivityAlias
+                        )
+                    }) {
+                        Text(text = "Big Icon")
+                    }
+                    Button(onClick = {
+                        changeEnabledComponent(
+                            mainActivityAlias,
+                            mainActivity
+                        )
+                    }) {
+                        Text(text = "Small Icon")
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    DynamicAppIconTheme {
-        Greeting("Android")
-    }
+fun Activity.changeEnabledComponent(
+    enabled: String,
+    disabled: String,
+) {
+    packageManager.setComponentEnabledSetting(
+        ComponentName(
+            this,
+            enabled
+        ),
+        PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+        PackageManager.DONT_KILL_APP
+    )
+    packageManager.setComponentEnabledSetting(
+        ComponentName(
+            this,
+            disabled
+        ),
+        PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+        PackageManager.DONT_KILL_APP
+    )
 }
